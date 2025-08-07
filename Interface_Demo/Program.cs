ccb28848045b3d33c5ea8e822a8969ac64c71265
Zheena26 <zibrahim018@st.ug.edu.gh>
2025-08-07 11:59:27 +0000

    Task 3: Interface demo with movable vehicles

---
+++ Interface_Demo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+interface IMovable
+{
+    void Move();
+}
+
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving.");
+    }
+}
+
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Choose a vehicle (car or bicycle): ");
+        string input = Console.ReadLine()?.Trim().ToLower();
+
+        IMovable vehicle;
+
+        if (input == "car")
+        {
+            vehicle = new Car();
+            vehicle.Move();
+        }
+        else if (input == "bicycle")
+        {
+            vehicle = new Bicycle();
+            vehicle.Move();
+        }
+        else
+        {
+            Console.WriteLine("Unknown vehicle. Exiting...");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
