20cb82c55d0ec2ee635deaf6e595dc254239b600
Zheena26 <zibrahim018@st.ug.edu.gh>
2025-08-07 11:49:20 +0000

    Task1:Inheritance and Method Overriding

---
+++ Inheritance_Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic animal sound.");
+    }
+}
+
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the name of the animal (Dog or Cat): ");
+        string input = Console.ReadLine()?.Trim().ToLower();
+
+        Animal animal;
+
+        if (input == "dog")
+        {
+            animal = new Dog();
+            animal.MakeSound();
+        }
+        else if (input == "cat")
+        {
+            animal = new Cat();
+            animal.MakeSound();
+        }
+        else
+        {
+            Console.WriteLine("Unknown animal. Exiting program...");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
+
